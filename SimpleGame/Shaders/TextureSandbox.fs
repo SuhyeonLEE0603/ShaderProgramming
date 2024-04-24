5f0771974caeb031749b9f773980484405a2bb46
Lee Su Hyeon <dltngus12323@gmail.com>
2024-04-24 19:08:19 +0900

    위아래 반전 텍스처 생성

--- SimpleGame/Shaders/TextureSandbox.fs
+++ SimpleGame/Shaders/TextureSandbox.fs
@@ -6,11 +6,27 @@ in vec2 v_TexPos;
 
 uniform sampler2D u_Texture;
 
-void main()
+void P1()
 {
 	float tx = v_TexPos.x;
 	float ty = fract(v_TexPos.y * 2);
 	vec2 newTexPos = vec2(tx, ty);
 
 	FragColor = texture(u_Texture, newTexPos);
+}
+
+void P2()
+{
+	float tx = v_TexPos.x;
+	float ty = 1 - abs(v_TexPos.y * 2 - 1);
+	vec2 newTexPos = vec2(tx, ty);
+
+	FragColor = texture(u_Texture, newTexPos);
+}
+
+
+void main()
+{
+	// P1();
+	P2();
 }
